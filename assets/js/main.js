051818df8c3f18a0494378d7213dbc3fa5b5e593
Isabel Pereira de Souza <contatobelsouza@gmail.com>
2022-09-28 12:10:00 -0300

    alteração

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//trabalhando com JS em um arquivo externo
+
+const nome = "Fiap";
+console.log(nome, "chamando variável criada com var");
+
+escreve("Exmplo de função nominal")
+
+//função nominal - udf
+function escreve (titulo){
+    alert(titulo);
+}
