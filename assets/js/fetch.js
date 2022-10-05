62620616cd714a0859179ed0f0cedb9638bc1a67
Isabel Pereira de Souza <contatobelsouza@gmail.com>
2022-10-05 12:03:59 -0300

    alterações

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -4,29 +4,39 @@
 //BLUR SIGNIFICA FOCO
 
 const cep = document.querySelector("#cep");
-cep.addEventListener("blur", (e) => {
+
+const showData = (result) => {
+  for (const campo in result) {
+    if (document.querySelector("#" + campo)) {
+      document.querySelector("#" + campo).value = result[campo];
+    }
+  }
+};
+
+cep.addEventListener("blur", async (e) => {
   let search = cep.value.replace("-", "");
   if (search.length < 8 || Number(search.value) === NaN) {
     alert("CEP inválido!");
     cep.focus();
     return;
   }
-
+  
   const options = {
     method: "GET",
     mode: "cors",
     cache: "default",
   };
-
-  fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-    .then((response) => {
-      response.json().then((result) => {
-        console.log(result);
-      });
-    })
-    .catch((err) => {
-      console.log(e.message);
-    });
+  // axios - biblioteca javascript que usa fetch e XHR.
+  try {
+    const resultado = await fetch(
+      `https://viacep.com.br/ws/${search}/json/`,
+      options
+    );
+    const json = await resultado.json();
+    showData(json);
+  } catch (error) {
+    console.log("Temos um problemas", error.message);
+  }
 });
 
 // Cross-Origin Resource Sharing
@@ -36,3 +46,14 @@ cep.addEventListener("blur", (e) => {
   //fetch vai gerar uma promise. Promise é uma promessa de que algo vai acontecer.
   // se der certo, ele vai retornar o resultado. Se der errado, ele vai retornar um erro.
   // PROMISE É UMA INTENÇÃO - Se der certo nós resolvemos a promise. Se der errado, nós rejeitamos a promise.
+  //   fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+//     .then((response) => {
+//       response.json().then((result) => {
+//         console.log(result);
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(e.message);
+//     });
+
+//outra forma de fazer o mesmo código acima é:
